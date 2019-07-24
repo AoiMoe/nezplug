b9b8b92135cb86fd089ecaa8562217209b4cef06
Takuya SHIOZAKI <AoiMoe@imou.to>
2019-07-25 05:05:40 +0900

    kick out UI dependent codes from nsfsdk
    nsfsdk should be UI independent.

--- src/common/nsfsdk/nsfsdk.h
+++ src/common/nsfsdk/nsfsdk.h
@@ -2,7 +2,6 @@
 extern "C" {
 #endif
 
-#include <windows.h>
 #include <stdlib.h>
 
 #ifndef NSFSDKAPI
@@ -21,19 +20,12 @@ void NSFSDKAPI NSFSDK_Volume(HNSF hnsf, unsigned uVolume);
 void NSFSDKAPI NSFSDK_Render(HNSF hnsf, void *bufp, unsigned buflen);
 void NSFSDKAPI NSFSDK_Terminate(HNSF hnsf);
 void NSFSDKAPI NSFSDK_GetFileInfo(char **p1, char **p2, char **p3, char **p4);
-void NSFSDKAPI NSFSDK_LoadSetting(HNSF hnsf, char *file);
 
 unsigned NSFSDKAPI NSFSDK_GetSongNo(HNSF hnsf);
 unsigned NSFSDKAPI NSFSDK_GetSongStart(HNSF hnsf);
 unsigned NSFSDKAPI NSFSDK_GetSongMax(HNSF hnsf);
 unsigned NSFSDKAPI NSFSDK_GetChannel(HNSF hnsf);
 unsigned NSFSDKAPI NSFSDK_GetFrequency(HNSF hnsf);
-
-void NSFSDKAPI NSFSDK_OpenFileInfoDlg(HINSTANCE p1, HWND p2);
-void NSFSDKAPI NSFSDK_OpenMemViewDlg(HINSTANCE p1, HWND p2);
-void NSFSDKAPI NSFSDK_OpenChMaskDlg(HINSTANCE p1, HWND p2);
-void NSFSDKAPI NSFSDK_OpenIOViewDlg(HINSTANCE p1, HWND p2);
-void NSFSDKAPI NSFSDK_OpenDumpDlg(HINSTANCE p1, HWND p2);
 
 #ifdef __cplusplus
 }
